95b4ed4aa6324185f9fc32354a2e4597f54ea2b7
tomttk <tualkrebstom@gmail.com>
2023-02-20 17:31:35 +0100

    Spawn Enemy

--- Source/MammothCoinCollector/SpaceShooterGameMode.cpp
+++ Source/MammothCoinCollector/SpaceShooterGameMode.cpp
@@ -2,4 +2,31 @@
 
 
 #include "SpaceShooterGameMode.h"
+#include "EnemyController.h"
 
+void ASpaceShooterGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ASpaceShooterGameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	GameTimer += DeltaTime;
+	EnemyTimer -= DeltaTime;
+	if (EnemyTimer <= 0.0f)
+	{
+		float difficultyPercentage = FMath::Min(GameTimer / TIME_TO_MINIMUM_INTERVAL, 1.0f);
+		EnemyTimer = MAXIMUM_INTERVAL - (MAXIMUM_INTERVAL - MINIMUM_INTERVAL) * difficultyPercentage;
+
+		UE_LOG(LogTemp, Warning, TEXT("Timer is %f"), EnemyTimer);
+
+		UWorld* World = GetWorld();
+		if (World)
+		{
+			FVector Location = FVector(600.0f, FMath::RandRange(-800.0f, 800.0f), 70.f);
+			World->SpawnActor<AEnemyController>(EnemyBlueprint, Location, FRotator::ZeroRotator);
+		}
+	}
+}
--- Source/MammothCoinCollector/SpaceShooterGameMode.h
+++ Source/MammothCoinCollector/SpaceShooterGameMode.h
@@ -13,5 +13,25 @@ UCLASS()
 class MAMMOTHCOINCOLLECTOR_API ASpaceShooterGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+	float MINIMUM_INTERVAL = 0.5f;
+	float MAXIMUM_INTERVAL = 2.0f;
+	float TIME_TO_MINIMUM_INTERVAL = 30.0f;
+
+public:
+	/** Called when the game starts. */
+	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void Tick(float DeltaSeconds) override;
+
+	UPROPERTY(EditAnywhere, Category = "Spawning")
+		TSubclassOf<class AEnemyController> EnemyBlueprint;
+
+	float EnemyTimer;
+	float GameTimer;
+
+protected:
+
+	int Score = 0;
 };
